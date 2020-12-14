509b13a635e576ddc678c7eb9ca0545039794307
Filip Papranec <fpapranecx@gmail.com>
2020-12-14 13:44:44 +0100

    edit build script

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,21 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const env_1 = __importDefault(require("./env"));
-const cors_1 = require("./middlewares/cors");
+const middlewares_1 = require("./middlewares");
+const router_1 = __importDefault(require("./router"));
 const app = express_1.default();
-app.use(express_1.default.json());
+// view engine setup
+app.set('views', path_1.default.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use(cors_1.cors());
-app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.send('Ahoj');
-}));
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.use(middlewares_1.cors());
+app.use(router_1.default);
 app.listen(env_1.default.port, () => {
     console.log(`server started at http://localhost:${env_1.default.port}`);
 });
