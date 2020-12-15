1cca5f916154138eb8364c6efa147d59ad6d17fa
Filip Papranec <fpapranecx@gmail.com>
2020-12-15 09:54:18 +0100

    add error message

--- src/router.ts
+++ src/router.ts
@@ -3,24 +3,42 @@ import { upload } from './middlewares'
 import fs from 'fs'
 import { processSvg } from './utils/processSvg'
 
+const DEFAULT_OPTIONS = {
+  title: 'Kontentino Svg Optimizer',
+}
+
 const baseRouter = Router()
 
 baseRouter.get('/', function (req, res, next) {
-  res.render('index', { title: 'Kontentino Svg Optimizer' })
+  res.render('index', { ...DEFAULT_OPTIONS })
 })
 
 baseRouter.post(
   '/optimize',
   upload.single('icon'),
   async function (req, res, next) {
-    const svgString = fs.readFileSync(req.file.path).toString()
-    const optimizedSvgString = await processSvg(svgString)
+    try {
+      if (!!req.file) {
+        const svgString = fs.readFileSync(req.file.path).toString()
+        const optimizedSvgString = await processSvg(svgString)
 
-    const newFileName = req.file.path.replace('.svg', '-optimized.svg')
+        const newFileName = req.file.path.replace('.svg', '-optimized.svg')
 
-    fs.writeFileSync(newFileName, optimizedSvgString)
+        fs.writeFileSync(newFileName, optimizedSvgString)
 
-    return res.download(newFileName, newFileName)
+        return res.download(newFileName, newFileName)
+      }
+
+      return res.render('index', {
+        ...DEFAULT_OPTIONS,
+        error: new Error('Wrong File Selected'),
+      })
+    } catch (e) {
+      return res.render('index', {
+        ...DEFAULT_OPTIONS,
+        error: new Error('Unknown Error'),
+      })
+    }
   }
 )
 
